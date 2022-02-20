a65299bea435a9970f3bf75c99c234b731088746
Naimur Rahman Nidrit <akash66445@gmail.com>
2022-02-20 18:10:50 +0600

    My first commit message1

--- mail.php
+++ mail.php
@@ -5,8 +5,8 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 $pm = new PHPMailer(true);
 try{
-    $pm ->setFrom("akash66445@gmail.com");
-    $pm ->addAddress("naimur404cse@gmail.com");
+    $pm ->setFrom("test@gmail.com");
+    $pm ->addAddress("test1@gmail.com");
     $pm ->Subject= "here is invoice test 2";
     $pm->Body= "<strong>Hi kmon asoo<strong>";
     $pm ->AltBody= "life onk pera";
